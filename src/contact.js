a747e9968b1659f3a0d5c67a6666e3b9e28b541a
krokhale <krishna.rokhale@gmail.com>
2022-02-15 17:49:45 -0800

    checking in

---
+++ src/contact.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function Contact(props) {
+    return (
+        <div>
+            contact component
+        </div>
+    );
+}
+
+export default Contact;
